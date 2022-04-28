46b07de70559dec7e1c4ba9cbef8cf0c1e3557b1
gtech <dimadim202@gmail.com>
2022-04-28 23:45:41 +0700

    push

--- app/src/main/java/com/example/mobile/Activity1.kt
+++ app/src/main/java/com/example/mobile/Activity1.kt
@@ -4,15 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 
-class MainActivity : AppCompatActivity() {
+class Activity1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_1)
 
-        val button1 = findViewById<Button>(R.id.button1)
-        button1.setOnClickListener {
-            val intent = Intent(this, Activity1::class.java)
+        val button3 = findViewById<ImageButton>(R.id.button3)
+        button3.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
